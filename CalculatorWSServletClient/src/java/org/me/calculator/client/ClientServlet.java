4da78e2ea2d78ec62226a84c2644cdca0081c6fa
safahope <34474625+safahope@users.noreply.github.com>
2017-12-12 20:29:37 +0100

    Add files via upload

---
+++ CalculatorWSServletClient/src/java/org/me/calculator/client/ClientServlet.java
@@ -0,0 +1,102 @@
+package org.me.calculator.client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+import org.me.calculator.CalculatorWS_Service;
+
+@WebServlet(name = "ClientServlet", urlPatterns = {"/ClientServlet"})
+public class ClientServlet extends HttpServlet {
+
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/CalculatorWSApplication/CalculatorWS.wsdl")
+    private CalculatorWS_Service service;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    try {
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet ClientServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Servlet ClientServlet at " + request.getContextPath () + "</h1>");
+        
+          out.println("Erreuuuuuuuuuuuuur normal ");
+        try {
+            int i = 3;
+            int j = 4;
+            int result = add(i, j);
+            out.println("Result = " + result);
+        } catch (Exception ex) {
+            out.println("Erreuuuuuuuuuuuuur " + ex);
+        }
+        out.println("</body>");
+        out.println("</html>");
+        
+    } finally {            
+        out.close();
+    }
+}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+private int add(int i, int j) {
+    org.me.calculator.CalculatorWS port = service.getCalculatorWSPort();
+    return port.add(i, j);
+}
+
+  
+
+}
